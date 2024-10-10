771d7f064c07ed6f56b8c168de56f16521a1c368
AnastasiaMoglan <160147583+AnastasiaMoglan@users.noreply.github.com>
2024-10-10 08:53:02 +0300

    main.py

--- main.py
+++ main.py
@@ -1,52 +1,92 @@
-class Book:
-    def __init__(self, title, author, isbn):
+class Publication:
+    def __init__(self, title):
         self.title = title
+
+    def get_title(self):
+        return self.title
+
+    def get_info(self):
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+
+class Book(Publication):
+    def __init__(self, title, author, isbn):
+        super().__init__(title)
         self.author = author
         self.isbn = isbn
 
-    def __str__(self):
-        return f"Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}"
+    def get_author(self):
+        return self.author
+
+    def get_isbn(self):
+        return self.isbn
+
+    def get_info(self):
+        return f"Book: {self.title}, Author: {self.author}, ISBN: {self.isbn}"
+
 
 class Library:
     def __init__(self):
         self.books = []
 
     def add_book(self, book):
-        if not isinstance(book, Book):
-            print("Error: You need to add a Book object.")
-            return
         self.books.append(book)
+        print(f"Book added: {book.get_title()}")
 
     def remove_book(self, isbn):
-        for book in self.books:
-            if book.isbn == isbn:
-                self.books.remove(book)
-                return True
-        return False
+        initial_size = len(self.books)
+        self.books = [book for book in self.books if book.get_isbn() != isbn]
+
+        if len(self.books) < initial_size:
+            print(f"Book with ISBN {isbn} has been removed.")
+        else:
+            print(f"Book with ISBN {isbn} not found.")
 
     def display_books(self):
         if not self.books:
             print("The library is empty.")
         else:
+            print("Books in the library:")
             for book in self.books:
-                print(book)
+                print(book.get_info())
+
+
+def display_menu():
+    print("\n -|- Library Menu -|-")
+    print("1. Add book")
+    print("2. Remove book by ISBN")
+    print("3. Display all books")
+    print("0. Exit")
+    return input("Choose an option: ")
+
+
+def main():
+    library = Library()
+
+    while True:
+        option = display_menu()
+
+        if option == '1':
+            title = input("Enter the book title: ")
+            author = input("Enter the book author: ")
+            isbn = input("Enter the book ISBN: ")
+            new_book = Book(title, author, isbn)
+            library.add_book(new_book)
+
+        elif option == '2':
+            isbn = input("Enter the ISBN of the book to remove: ")
+            library.remove_book(isbn)
+
+        elif option == '3':
+            library.display_books()
+
+        elif option == '0':
+            print("Exiting the program.")
+            break
+
+        else:
+            print("Invalid option! Please choose again.")
+
 
 if __name__ == "__main__":
-    library = Library()
-    
-    book1 = Book("1984", "George Orwell", "1234567890")
-    book2 = Book("Pride and Prejudice", "Jane Austen", "0987654321")
-    
-    library.add_book(book1)
-    library.add_book(book2)
-    
-    print("Books in the library:")
-    library.display_books()
-
-    if library.remove_book("1234567890"):
-        print("\nBook removed successfully.")
-    else:
-        print("\nBook not found.")
-    
-    print("\nBooks after removal:")
-    library.display_books()
+    main()
